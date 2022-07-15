420bd3f457f1ef9e61355878755343e244c1b767
Macro <fuhong.zhou@qq.com>
2022-07-15 18:58:23 +0800

    .

--- DKControllerWPF/MainWindow.xaml.cs
+++ DKControllerWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DKCommunication.BasicFramework;
 using DKCommunication.Dandick.DK81Series;
+using DKCommunication.Core;
 using System;
 using System.IO.Ports;
 using System.Windows;
@@ -18,6 +19,8 @@ namespace DKControllerWPF
             InitializeComponent();
             DataContext = this;
             innit();
+            cbxDataFormat.ItemsSource = Enum.GetNames(typeof(DKCommunication.Core.DataFormat));
+            cbxDataFormat.SelectedIndex = (int)dandick.ByteTransform.DataFormat;
         }
 
         void innit()
@@ -30,6 +33,15 @@ namespace DKControllerWPF
                 cbxSerialPortsNames.Items.Add(port);
             }
             cbxSerialPortsNames.SelectedIndex = 0;
+
+            //初始化显示页面设置列表框
+            cbxSetDisplayPage.ItemsSource = Enum.GetNames(typeof(DisplayPage));
+            cbxSetDisplayPage.SelectedIndex = (int)dandick.DisplayPage;
+
+            //初始化接线模式设置列表框
+            cbxSetWireMode.ItemsSource=Enum.GetNames(typeof(WireMode));
+            cbxSetWireMode.SelectedIndex = (int)dandick.WireMode;
+
         }
 
         /// <summary>
@@ -58,7 +70,7 @@ namespace DKControllerWPF
                 txbVersion.Text = dandick.Version.ToString();
                 txbSerialNO.Text = dandick.SN;
                 txbModel.Text = dandick.Model;
-            }           
+            }
         }
 
         /// <summary>
@@ -74,19 +86,19 @@ namespace DKControllerWPF
                 return;
             }
             var d = dandick.ReadACSourceRanges();
-            ReadACSourceRangesIsSuccess.Text=d.IsSuccess.ToString();
-            ReadACSourceRangesErrorCode.Text=d.ErrorCode.ToString();
+            ReadACSourceRangesIsSuccess.Text = d.IsSuccess.ToString();
+            ReadACSourceRangesErrorCode.Text = d.ErrorCode.ToString();
             ReadACSourceRangesMesseage.Text = d.Message;
             if (d.IsSuccess)
             {
-                ReadACSourceRangesResponse.Text = SoftBasic.ByteToHexString(d.Content,' ');
-                txbURangeCount.Text=dandick.ACU_RangesCount.ToString();
+                ReadACSourceRangesResponse.Text = SoftBasic.ByteToHexString(d.Content, ' ');
+                txbURangeCount.Text = dandick.ACU_RangesCount.ToString();
                 txbIRangeCount.Text = dandick.ACI_RangesCount.ToString();
                 txbIProtectRangeCount.Text = dandick.IProtectRangesCount.ToString();
-                txbURangsASingle.Text=dandick.URanges_Asingle.ToString();
+                txbURangsASingle.Text = dandick.URanges_Asingle.ToString();
                 txbIRangsASingle.Text = dandick.IRanges_Asingle.ToString();
                 txbIProtectRangsASingle.Text = dandick.IProtectRanges_Asingle.ToString();
-                cbxACURangs.ItemsSource = dandick.ACU_RangesList;                
+                cbxACURangs.ItemsSource = dandick.ACU_RangesList;
                 cbxACIRangs.ItemsSource = dandick.ACI_RangesList;
                 cbxACIRangs.SelectedIndex = 0;
                 cbxACURangs.SelectedIndex = 0;
@@ -126,7 +138,7 @@ namespace DKControllerWPF
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-            }           
+            }
         }
 
         private void btnrefresh_Click(object sender, RoutedEventArgs e)
@@ -155,7 +167,7 @@ namespace DKControllerWPF
             if (d.IsSuccess)
             {
                 ReadDCSourceRangesResponse.Text = SoftBasic.ByteToHexString(d.Content, ' ');
-              
+
                 txbDCURangeCount.Text = dandick.DCI_RangesCount.ToString();
                 txbDCIRangeCount.Text = dandick.DCI_RangesCount.ToString();
                 cbxDCURangs.ItemsSource = dandick.DCU_Ranges;
@@ -182,12 +194,109 @@ namespace DKControllerWPF
             if (d.IsSuccess)
             {
                 ReadDCSourceRangesResponse.Text = SoftBasic.ByteToHexString(d.Content, ' ');
-              
+
                 txbDCMURangeCount.Text = dandick.DCM_URangesCount.ToString();
                 txbDCMIRangeCount.Text = dandick.DCM_IRangesCount.ToString();
                 cbxDCMURangs.ItemsSource = dandick.DCM_URanges;
                 cbxDCMIRangs.ItemsSource = dandick.DCM_IRanges;
             }
+        }
+
+        private void cbxDataFormat_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            dandick.ByteTransform.DataFormat = (DKCommunication.Core.DataFormat)cbxDataFormat.SelectedIndex;
+        }
+
+        /// <summary>
+        /// 设置当前显示页面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_SetDisplayPage(object sender, RoutedEventArgs e)
+        {
+            if (!dandick.IsOpen())
+            {
+                MessageBox.Show("串口都没打开，你读个嘚啊！");
+                return;
+            }
+            var d = dandick.SetDisplayPage((DisplayPage)Enum.Parse(typeof(DisplayPage), cbxSetDisplayPage.SelectedItem.ToString()));
+            tbxSetDisplayPageErrorCode.Text = d.ErrorCode.ToString();
+            tbxSetDisplayPageIsSuccess.Text = d.IsSuccess.ToString();
+            tbxSetDisplayPageMesseage.Text = d.Message;
+            if (d.IsSuccess)
+            {
+                tbxSetDisplayPage.Text = SoftBasic.ByteToHexString(d.Content,' ');
+            }
+            tbxSetDisplayPage.Text = dandick.DisplayPage.ToString();
+        }
+
+        /// <summary>
+        /// 交流源关闭命令
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_StopACSource(object sender, RoutedEventArgs e)
+        {
+            if (!dandick.IsOpen())
+            {
+                MessageBox.Show("串口都没打开，你读个嘚啊！");
+                return;
+            }
+
+            var d=dandick.StopACSource();
+            tbxStopACSourceErrorCode.Text = d.ErrorCode.ToString();
+            tbxStopACSourceIsSuccess.Text = d.IsSuccess.ToString();
+            tbxStopACSourceMesseage.Text = d.Message;
+            if (d.IsSuccess)
+            {
+                tbxStopACSourceeResponse.Text = SoftBasic.ByteToHexString(d.Content, ' ');
+            }
+        }
+
+        /// <summary>
+        /// 交流源打开命令
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_StarACSource(object sender, RoutedEventArgs e)
+        {
+            if (!dandick.IsOpen())
+            {
+                MessageBox.Show("串口都没打开，你读个嘚啊！");
+                return;
+            }
+
+            var d = dandick.StartACSource();
+            tbxStartACSourceErrorCode.Text = d.ErrorCode.ToString();
+            tbxStartACSourceIsSuccess.Text = d.IsSuccess.ToString();
+            tbxStartACSourceMesseage.Text = d.Message;
+            if (d.IsSuccess)
+            {
+                tbxStartACSourceResponse.Text = SoftBasic.ByteToHexString(d.Content, ' ');
+            }
+        }
+
+        /// <summary>
+        /// 设置接线模式
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_SetWireMode(object sender, RoutedEventArgs e)
+        {
+            if (!dandick.IsOpen())
+            {
+                MessageBox.Show("串口都没打开，你读个嘚啊！");
+                return;
+            }
+            var d = dandick.SetWireMode((WireMode)Enum.Parse(typeof(WireMode), cbxSetWireMode.SelectedItem.ToString()));
+            tbxSetWireModeErrorCode.Text = d.ErrorCode.ToString();
+            tbxSetWireModeIsSuccess.Text = d.IsSuccess.ToString();
+            tbxSetWireModeMesseage.Text = d.Message;
+            if (d.IsSuccess)
+            {
+                tbxSetWireModeResponse.Text = SoftBasic.ByteToHexString(d.Content, ' ');
+            }
+            tbxSetWireModePage.Text = dandick.WireMode.ToString();
         }
     }
 }
